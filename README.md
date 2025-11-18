f42b0973985060ddcdd4269c5663791668049ee2
Siddik-001 <absiddik109816@gmail.com>
2025-11-19 02:21:11 +1100

    done

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-hello world
+<!-- *?What are some differences between interfaces and types in TypeScript? -->
+
+Interfaces and types both are used to declare types but there are some differences between them:
+
+1. Interface only works for object type data like: array, object,function.
+But it is not valid for primitive type data like string, number or boolean since interface has to be declared like this: 
+    interface name{
+
+    }
+But in type aliasing we can store primitive type data like this: 
+    type name = string;
+
+2. If we want we can merge the properties of type aliases by using intersection , where in case of interface we cannot merge but we can extend the properties of one interface to another like this: 
+    interface user extends name{
+
+    }
+
+
+<!-- *?What is the use of the keyof keyword in TypeScript? Provide an example. -->
+
+keyof is a type operator which means it works on types. If we just need the keys of an object type then we can use this operator.
+A very good usage of this could be- when we want to take an object and a key as an input of a function to return the value of that key in that object.
+
+    type Toys={
+        car: string;
+        bike: string;
+        plane: string;
+    }
+    type MyToy = keyof toys; // car|bike|plane
+
+    function getToy(obj: Toys, key: MyToy){
+        return obj[key];
+    }
+
+    const toys: Toys ={
+        car : "Yamaha",
+        bike: "s",
+        plane: "a"
+    }
+
+    const myToy: string = "bike";
+
+    console.log(getToy(toys,myToy))
+
+Here in the function, for the parametre "key" we declared MyToy as type which is quite safe as compare to string , as it won't accept any string other than car,bike,plane. So there is no chance of error in the return line and if a user is inputting a wrong key they will get it in the very first place.
--- Solution.ts
+++ Solution.ts
@@ -1,5 +1,179 @@
-{
-    const name: string = " Hello World";
-    const nname: string = " Hello World";
-    console.log(name)
+
+
+function formatValue(value: string | number | boolean) {
+    if (typeof value === "string" && value.length > 0) {
+        return value.toUpperCase();
+    } else if (typeof value === "number") {
+        return value * 10;
+    } else if (typeof value === "boolean") {
+        return !value;
+    }
+}
+
+
+
+
+
+function getLength(value: string | (string | number | boolean)[]) {
+    if (typeof value === "string" && value.length > 0) {
+
+        return value.replace(/[^a-zA-Z0-9]/g, "").length; 
+    }
+    else if (Array.isArray(value)) {
+        let size = value.filter(i => !/[^a-zA-Z0-9]/.test(String(i))).length;
+
+        return size;
+    }
+}
+
+
+
+
+class Person {
+    constructor(public name: string, public age: number) {
+
+    }
+    getDetails() {
+        return `'Name: ${this.name}, Age: ${this.age}'`
+    }
+}
+
+
+
+
+
+
+
+type Person_Obj = {
+    title: string,
+    rating: number
+}
+function filterByRating(value: Person_Obj[]) {
+    let new_arr = value.filter((item) => { return item.rating >= 4 })
+    // {} use krsi tai return likhte hobe 
+    return new_arr;
+}
+
+
+
+
+
+type User = {
+    id: number,
+    name: string,
+    email: string,
+    isActive: boolean
+}
+function filterActiveUsers(value: User[]) {
+    let updated = value.filter(i => i.isActive === true)
+    return updated;
+}
+
+
+
+
+
+
+
+interface Book {
+    title: string,
+    author: string,
+    publishedYear: number,
+    isAvailable: boolean
+}
+
+
+function printBookDetails(value: Book) {
+    value.isAvailable ?
+        console.log(`Title: ${value.title}, Author: ${value.author}, Published: ${value.publishedYear}, Available: Yes`)
+        :
+        console.log(`Title: ${value.title}, Author: ${value.author}, Published: ${value.publishedYear}, Available: No`)
+}
+
+
+
+
+
+
+
+
+type arr = (string | number)[]
+
+function getUniqueValues(arr1: arr, arr2: arr) {
+    let new_arr: arr = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+
+        if (typeof arr1[i] === "string") {
+            if ((arr1[i] as string).trim() !== "") {
+
+                new_arr[new_arr.length] = arr1[i];
+            }
+        } else {
+            new_arr[new_arr.length] = arr1[i];
+
+        }
+    }
+    for (let i = 0; i < arr2.length; i++) {
+
+        if (typeof arr2[i] === "string") {
+            if ((arr2[i] as string).trim() !== "") {
+
+                new_arr[new_arr.length] = arr2[i];
+            }
+        } else {
+            new_arr[new_arr.length] = arr2[i];
+
+        }
+    }
+
+    let unique: arr = [];
+
+    for (let i = 0; i < new_arr.length; i++) {
+        let exists = false;
+        for (let j = 0; j < unique.length; j++) {
+            if (new_arr[i] === unique[j]) {
+                exists = true;
+                break
+            }
+        }
+
+        if (!exists) {
+            unique[unique.length] = new_arr[i] as string | number;
+        }
+    }
+
+    return unique;
+
+}
+
+
+
+
+
+
+
+
+type Obj = {
+    name: string,
+    price: number,
+    quantity: number,
+    discount?: number
+}
+
+function calculateTotalPrice(value: Obj[]) {
+
+    let total: number[] = value.map((i) => {
+        if (i.discount) {
+            return (i.price * i.quantity) - (i.price * i.quantity) * (i.discount / 100)
+        } else {
+            return (i.price * i.quantity)
+        }
+    })
+
+    let sum = 0;
+    for (let x = 0; x < total.length; x++) {
+        sum += total[x];
+    }
+    return sum;
 }
